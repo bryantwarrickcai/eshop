e37c3c8ce771e5579a9a2b88a6e2f6adef155673
bryantwarrickcai <bryantwarrickcai@gmail.com>
2025-03-05 16:19:22 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -14,14 +14,73 @@ public class Payment {
     Map<String, String> paymentData;
 
     public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.status = status;
+        this.paymentData = paymentData;
     }
 
     public Payment(String id, String method, Map<String, String> paymentData) {
+        this(id, null, null, paymentData);
+        this.setMethod(method);
+        this.setStatus();
     }
 
     public void setMethod(String method) {
+        String[] methods = {"VOUCHER", "CASH_ON_DELIVERY"};
+
+        boolean found = false;
+        for (String methodName : methods) {
+            if (method.equals(methodName)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            this.method = method;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void setStatus() {
+        // Check the type of payment method
+        if (this.method.equals("VOUCHER")) {
+            if (this.paymentData.size() != 1 || !this.paymentData.containsKey("voucherCode")) {
+                this.status = "REJECTED";
+            } else {
+                String voucherCode = this.paymentData.get("voucherCode");
+
+                int numberOfNumericCharacters = 0;
+                for (char c : voucherCode.toCharArray()) {
+                    if (Character.isDigit(c)) {
+                        numberOfNumericCharacters++;
+                    }
+                }
+
+                if (voucherCode.length() == 16 &&
+                        voucherCode.substring(0, 5).equals("ESHOP") &&
+                        numberOfNumericCharacters == 8) {
+                    this.status = "SUCCESS";
+                } else {
+                    this.status = "REJECTED";
+                }
+            }
+        } else if (this.method.equals("CASH_ON_DELIVERY")) {
+            if (this.paymentData.size() != 2 ||
+                    !this.paymentData.containsKey("address") ||
+                    !this.paymentData.containsKey("deliveryFee")) {
+                this.status = "REJECTED";
+            } else {
+                String address = this.paymentData.get("address");
+                String deliveryFee = this.paymentData.get("deliveryFee");
+                if (address == null || address.isEmpty() || deliveryFee == null || deliveryFee.isEmpty()) {
+                    this.status = "REJECTED";
+                } else {
+                    this.status = "SUCCESS";
+                }
+            }
+        }
     }
 }
